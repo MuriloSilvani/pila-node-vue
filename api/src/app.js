2a5daabdc945743d695bc595f09f529ee92cd4f4
Murilo Ubuntu <murilo@amo.delivery>
2020-11-03 22:18:42 -0300

    socket

--- api/src/app.js
+++ api/src/app.js
@@ -2,12 +2,27 @@ const express = require('express')
 const cors = require('cors')
 
 const routes = require('./routes')
-
 const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const connectedUsers = {}
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query
+  connectedUsers[user] = socket.id
+})
+io.on('move', socket => {
+  console.log(socket)
+})
+// app.use((req, res, next) => {
+//   req.io = io
+//   req.connectedUsers = connectedUsers
+//   return next()
+// })
 
 app.use(cors())
 app.use(express.json())
-
 app.use(routes)
 
-app.listen(3000)
+server.listen(3000)
--- web/src/components/Dashboard/index.vue
+++ web/src/components/Dashboard/index.vue
@@ -1,11 +1,71 @@
 <template>
-  <div>
-    Dashboard
+  <div
+    style='height: calc(100vh - 100px); position: relative;'
+  >
+    <button v-if="!user" @click='login'>Login</button>
+    <span v-if="user">{{ user.name }} {{user.x}}</span>
+    <div
+      style='height: 100%; border: 1px solid black; position: relative;'
+    >
+      <div
+        v-if="user"
+        :style="`position: absolute; height: 1%; width: 1%; background: red; left: ${user.x}%; top: ${user.y}%;`"
+      >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import io from 'socket.io-client'
+
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+  data () {
+    return {
+      socket: null,
+      user: {
+        name: 'Murilo',
+        x: 0,
+        y: 0
+      }
+    }
+  },
+  mounted () {
+    this.socket = io('http://localhost:3000', {
+      query: {
+        user: navigator.userAgent.toLowerCase(),
+        navigator: navigator
+      }
+    })
+    document.addEventListener('keydown', this.move)
+  },
+  methods: {
+    move (e) {
+      if (!this.user) {
+        return
+      }
+      const code = e.keyCode
+
+      if (code === 37) {
+        this.user.x -= this.user.x > 0 ? 1 : 0
+      }
+      if (code === 38) {
+        this.user.y -= this.user.y > 0 ? 1 : 0
+      }
+      if (code === 39) {
+        this.user.x += this.user.x < 99 ? 1 : 0
+      }
+      if (code === 40) {
+        this.user.y += this.user.y < 99 ? 1 : 0
+      }
+    },
+    login () {
+      this.user = {}
+      this.user.name = prompt('Your name')
+      this.user.x = 0
+      this.user.y = 0
+    }
+  }
 }
 </script>
